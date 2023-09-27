8789d1efd93a94a6a56f428c48bc220db029c8cc
Samuel Frossard <samuel.frossard79@gmail.com>
2023-09-27 11:20:39 +0200

    Tuto fini

--- movie/movie.py
+++ movie/movie.py
@@ -1,6 +1,6 @@
 from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, QueryType, MutationType
 from ariadne.constants import PLAYGROUND_HTML
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 
 import resolvers as r
 
@@ -22,10 +22,31 @@ def home():
 @app.route('/graphql', methods=['GET'])
 def playground():
     return PLAYGROUND_HTML, 200
-    
+
+type_defs = load_schema_from_path('movie.graphql')
+query = QueryType()
+movie = ObjectType('Movie')
+query.set_field('movie_with_id', r.movie_with_id)
+schema = make_executable_schema(type_defs, movie, query)
 @app.route('/graphql', methods=['POST'])
 def graphql_server():
-    # todo to complete
+    data = request.get_json()
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=None,
+        debug=app.debug
+    )
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
+
+mutation = MutationType()
+mutation.set_field('update_movie_rate', r.update_movie_rate)
+schema = make_executable_schema(type_defs, movie, query, mutation)
+
+actor = ObjectType('Actor')
+movie.set_field('actors', r.resolve_actors_in_movie)
+schema = make_executable_schema(type_defs, movie, query, mutation, actor)
 
 if __name__ == "__main__":
     print("Server running in port %s"%(PORT))
