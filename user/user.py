04c27b9e264a34ea36933a05f4f363d669efd5c2
Samuel Frossard <samuel.frossard79@gmail.com>
2023-10-11 09:33:48 +0200

    user service with rest implemented

--- user/user.py
+++ user/user.py
@@ -1,27 +1,63 @@
-# REST API
 from flask import Flask, render_template, request, jsonify, make_response
 import requests
 import json
 from werkzeug.exceptions import NotFound
 
-# CALLING gRPC requests
-import grpc
-from concurrent import futures
-import booking_pb2
-import booking_pb2_grpc
-import movie_pb2
-import movie_pb2_grpc
-
-# CALLING GraphQL requests
-# todo to complete
-
 app = Flask(__name__)
 
-PORT = 3004
+PORT = 3203
 HOST = '0.0.0.0'
 
-with open('{}/data/users.json'.format("."), "r") as jsf:
+with open('{}/databases/users.json'.format("."), "r") as jsf:
    users = json.load(jsf)["users"]
+
+@app.route("/", methods=['GET'])
+def home():
+   return make_response("<h1 style='color:blue'>Welcome to the User service!</h1>", 200)
+
+@app.route("/users", methods=['GET'])
+def get_json():
+    res = make_response(jsonify(users), 200)
+    return res
+
+@app.route("/users/<userid>", methods=['GET'])
+def get_user_by_id(userid):
+    for user in users:
+        if str(user["id"]) == str(userid):
+            res = make_response(jsonify(user),200)
+            return res
+    return make_response(jsonify({"error":"bad input parameter"}),400)
+
+@app.route("/users/bookings/<userid>", methods=['GET', 'POST'])
+def get_bookings_by_userid(userid):
+    bookings = requests.get('http://172.16.132.100:3201/bookings').json()
+    for booking in bookings:
+        if str(booking["userid"]) == str(userid):
+            res = make_response(jsonify(booking["dates"]),200)
+            return res
+    return make_response(jsonify({"error":"no booking found"}),400)
+
+@app.route("/users/movies/<userid>", methods=['GET'])
+def get_movies_by_userid(userid):
+    print("userid", userid)
+    user_bookings_request = requests.get('http://172.16.132.100:3203/users/bookings/'+userid)
+    user_bookings = user_bookings_request.json()
+    print("user_bookings", user_bookings)
+    if user_bookings_request.status_code == 200:
+        moviesInfo = {
+            "movies":[]
+        }
+        for booking in user_bookings:
+            for movieid in booking["movies"]:
+                print("movieid", movieid)
+                print('http://172.16.132.100:3200/movies/'+movieid)
+                movie_request = requests.get('http://172.16.132.100:3200/movies/'+movieid)
+                movie = movie_request.json()
+                if movie_request.status_code ==200:
+                    print("movie", movie)
+                    moviesInfo["movies"].append(movie)
+        return make_response(jsonify(moviesInfo),200)
+    return make_response(jsonify({"error": "no booking found for user" + userid}), 400)
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
