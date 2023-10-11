13c02161bcabda1d45e796921516dc296735316f
Camille <camille.pinton@imt-atlantique.net>
2023-10-11 09:55:27 +0200

    gRPC Booking

--- user/user.py
+++ user/user.py
@@ -9,11 +9,25 @@ import grpc
 from concurrent import futures
 import booking_pb2
 import booking_pb2_grpc
-import movie_pb2
-import movie_pb2_grpc
 
 # CALLING GraphQL requests
-# todo to complete
+
+
+
+def get_bookings_by_userid(stub, userid):
+   bookings = stub.GetBookingsByUserId(userid)
+   print(userid)
+   for booking in bookings :
+      print(booking)
+
+def run():
+   with grpc.insecure_channel('localhost:3002') as channel:
+      stub = booking_pb2_grpc.BookingStub(channel)
+
+      print("-------------- GetBookingsByUserId --------------")
+      userid = booking_pb2.UserId(userid="dwight_schrute")
+      get_bookings_by_userid(stub, userid)
+
 
 app = Flask(__name__)
 
@@ -24,5 +38,7 @@ with open('{}/data/users.json'.format("."), "r") as jsf:
    users = json.load(jsf)["users"]
 
 if __name__ == "__main__":
+   run()
    print("Server running in port %s"%(PORT))
    app.run(host=HOST, port=PORT)
+
